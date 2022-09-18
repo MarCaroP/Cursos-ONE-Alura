008a136eb5214faeb60bb149fc00916947c529da
MarC <caropompolomaria@gmail.com>
2022-09-18 18:40:24 -0300

    Organizo metodos y archivos js

--- JS Almacenando datos en navegador/components/addTask.js
+++ JS Almacenando datos en navegador/components/addTask.js
@@ -2,41 +2,46 @@ import checkComplete from './checkComplete.js';
 import deleteIcon from './deleteIcon.js';
 
 export const addTask = (evento)=>{
-    const list = document.querySelector('[data-list]');
-    const task = createTask(evento);
-    list.appendChild(task);
-    };
-    
-    const createTask = (evento) => {
     evento.preventDefault();
-    const taskList=JSON.parse(localStorage.getItem('tasks'))|| [];
-    const input = document.querySelector('[data-form-input]');
+
+    const list = document.querySelector('[data-list]');
+    const input = document.querySelector('[data-form-input]');    
     const calendar= document.querySelector('[data-form-date]');
     const value = input.value;
     const date= calendar.value; 
     const dateFormat= moment(date).format('DD/MM/YYYY');
-    const task = document.createElement('li');
-    task.classList.add('card');
+
     input.value = '';
-    //backticks
-    const taskContent = document.createElement('div');
+    calendar.value='';
     const taskObject={
         value,
         dateFormat
     };
-    taskList.push(taskObject);
-    localStorage.setItem('tasks',JSON.stringify(taskList));
+
+    const taskList=JSON.parse(localStorage.getItem('tasks'))|| [];
+        taskList.push(taskObject);
+        localStorage.setItem('tasks',JSON.stringify(taskList));
+
+    const task = createTask(taskObject);
+    list.appendChild(task);
+    };
+    
+export const createTask = ({value,dateFormat}) => {
+    const task = document.createElement('li');
+        task.classList.add('card');
+
+    const taskContent = document.createElement('div');
     const titleTask = document.createElement('span');
-    titleTask.classList.add('task');
-    titleTask.innerText = value;
-    taskContent.appendChild(checkComplete());
-    taskContent.appendChild(titleTask);
-    // task.innerHTML = content;
+        titleTask.classList.add('task');
+        titleTask.innerText = value;
+        taskContent.appendChild(checkComplete());
+        taskContent.appendChild(titleTask);
+
     const dateElement= document.createElement('span');
-    dateElement.innerHTML= dateFormat;
-    task.appendChild(taskContent);
-    task.appendChild(dateElement);
-    task.appendChild(deleteIcon());
+        dateElement.innerHTML= dateFormat;
+        task.appendChild(taskContent);
+        task.appendChild(dateElement);
+        task.appendChild(deleteIcon());
 
     return task;
     };
---
+++ JS Almacenando datos en navegador/components/readTasks.js
@@ -0,0 +1,10 @@
+import { createTask } from "./addTask.js";
+
+export const readTasks = ()=>{
+    const list= document.querySelector('[data-list]');
+    const tasksList= JSON.parse(localStorage.getItem('tasks'))||[];
+    
+    tasksList.forEach((task) => {
+        list.appendChild(createTask(task));        
+    });
+};
